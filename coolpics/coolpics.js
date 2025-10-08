e55c423abba5c996497a38641252f90afabab832
gaydeym <gay20001@byui.com>
2025-10-08 00:47:04 -0500

    Cool Pics part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,4 +7,43 @@ document.addEventListener("DOMContentLoaded", () => {
             navUl.classList.toggle("open");
         });
     }
+
+    const gallery = document.querySelector(".gallery");
+
+    if (gallery) {
+        gallery.addEventListener("click", handleGalleryClick);
+    }
+
+    function handleGalleryClick(event) {
+        const clickedImg = event.target.closest("img");
+        if (!clickedImg) return;
+
+        const src = clickedImg.src;
+        const alt = clickedImg.alt;
+
+        const fullSrc = src.split("-")[0] + "-full.jpeg";
+        const dialog = document.createElement("dialog");
+
+        dialog.innerHTML = `
+            <img src="${fullSrc}" alt="${alt}">
+            <button class="close-viewer">X</button>
+        `;
+
+        document.body.appendChild(dialog);
+
+        dialog.showModal();
+
+        const closeButton = dialog.querySelector(".close-viewer");
+        closeButton.addEventListener("click", () => {
+            dialog.close();
+            dialog.remove();
+        });
+
+        dialog.addEventListener("click", (event) => {
+            if (event.target === dialog) {
+                dialog.close();
+                dialog.remove();
+            }
+        });
+    }
 });
